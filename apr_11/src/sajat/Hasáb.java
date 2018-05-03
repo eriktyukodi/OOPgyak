40e391f497653782a7498184be7dcb4a8ce15200
Én <n@DESKTOP-A4O8D9T>
2018-05-03 14:32:22 +0200

    április 11 gyakorlat java

---
+++ apr_11/src/sajat/Hasáb.java
@@ -0,0 +1,26 @@
+package sajat;
+
+public abstract class Hasáb {
+	
+	protected int magassag;
+
+	public Hasáb(int magassag) {
+		this.magassag = magassag;
+	}
+
+	public int getMagassag() {
+		return magassag;
+	}
+	
+	public abstract int getAlapterulet();
+		
+	public int getTerfogat() {
+		return getAlapterulet() * magassag;
+	}
+
+	public boolean összehasolítás(Hasáb masik){
+		return (getTerfogat() > masik.getTerfogat());
+
+	}
+
+}
